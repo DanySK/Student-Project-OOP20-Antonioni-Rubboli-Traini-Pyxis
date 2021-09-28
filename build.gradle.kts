8a5d1167e1e5f69dc83f2ed3e78898c0d258242e
Danilo Pianini <danilo.pianini@unibo.it>
2021-09-28 18:38:43 +0200

    Fix broken build

--- build.gradle.kts
+++ build.gradle.kts
@@ -249,40 +249,38 @@ class CheckstyleQAInfoExtractor(root: org.w3c.dom.Element) : QAInfoContainer by 
 )
 
 class SpotBugsQAInfoExtractor(root: org.w3c.dom.Element) : QAInfoContainer by (
-    root.childNodes.let { childNodes ->
-        val sourceDirs = childNodes.toIterable()
-            .filter { it.nodeName == "Project" }
-            .first()
-            .childrenNamed("SrcDir")
-            .map { it.textContent.trim() }
-            .asSequence()
-        childNodes.toIterable()
-            .asSequence()
-            .filter { it.nodeName == "BugInstance" }
-            .mapNotNull { bugDescriptor ->
-                val sourceLineDescriptor = bugDescriptor.childrenNamed("SourceLine").first()
-                val category = bugDescriptor["category"].takeUnless { it == "STYLE" } ?: "UNSAFE"
-                val startLine = sourceLineDescriptor["start", "1"].toInt()
-                val endLine = sourceLineDescriptor["end", Integer.MAX_VALUE.toString()].toInt()
-                val actualFile = sourceLineDescriptor.get("relSourcepath") {
-                    val sourcePath = sourceLineDescriptor["sourcepath"]
-                    val potentialFiles = sourceDirs.map { "$it${File.separator}$sourcePath" }
-                    val existingCandidates = potentialFiles.filter { File(it).exists() }
-                    existingCandidates.firstOrNull() ?: "".also {
-                        logger.warn("Skipping file $sourcePath, as none of ${potentialFiles.toList()} exists")
+        root.childNodes.let { childNodes ->
+            val sourceDirs = childNodes.toIterable()
+                .filter { it.nodeName == "Project" }
+                .first()
+                .childrenNamed("SrcDir")
+                .map { it.textContent.trim() }
+                .asSequence()
+            childNodes.toIterable()
+                .asSequence()
+                .filter { it.nodeName == "BugInstance" }
+                .mapNotNull { bugDescriptor ->
+                    val sourceLineDescriptor = bugDescriptor.childrenNamed("SourceLine").first()
+                    val category = bugDescriptor["category"].takeUnless { it == "STYLE" } ?: "UNSAFE"
+                    val startLine = sourceLineDescriptor["start", "1"].toInt()
+                    val endLine = sourceLineDescriptor["end", Integer.MAX_VALUE.toString()].toInt()
+                    val candidateFile = sourceLineDescriptor.get("relSourcepath") {
+                        sourceLineDescriptor["sourcepath"]
+                    }
+                    val actualFile = sourceDirs.flatMap { File(it).walkTopDown() }
+                        .map { it.absolutePath }
+                        .first { candidateFile in it }
+                    actualFile.takeIf { it.isNotBlank() }?.let {
+                        QAInfoForChecker(
+                            "Potential bugs",
+                            actualFile,
+                            startLine..endLine,
+                            "[$category] ${bugDescriptor.childrenNamed("LongMessage").first().textContent.trim()}",
+                        )
                     }
                 }
-                actualFile.takeIf { it.isNotBlank() }?.let {
-                    QAInfoForChecker(
-                        "Potential bugs",
-                        actualFile,
-                        startLine..endLine,
-                        "[$category] ${bugDescriptor.childrenNamed("LongMessage").first().textContent.trim()}",
-                    )
-                }
-            }
-            .asIterable()
-    }
+                .asIterable()
+        }
 )
 
 fun org.w3c.dom.NodeList.toIterable() = Iterable {
